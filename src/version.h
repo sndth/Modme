a86eb1c167166c51a09a46ecc560dd118e5b9dc6
Thorek <75499293+sndth@users.noreply.github.com>
2026-09-11 19:51:49 +0200

    Bump version to 0.1.0 (#10)

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
 #ifndef MODME_VERSION_H
 #define MODME_VERSION_H
 
-#define MODME_VERSION "0.0.0"
+#define MODME_VERSION "0.1.0"
 #define MODME_VERSION_MAJOR 0
-#define MODME_VERSION_MINOR 0
+#define MODME_VERSION_MINOR 1
 #define MODME_VERSION_PATCH 0
 
 #endif
